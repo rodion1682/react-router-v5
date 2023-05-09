1c7f53363628c8ac5ca67f9c10841e64ccde7534
rodion1682 <rodiontropkin17@gmail.com>
2023-05-09 18:33:08 +0300

    v6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import {
-	BrowserRouter,
-	Redirect,
-	Route,
-	Switch,
-	useRouteMatch,
 	useParams,
 	NavLink,
+	useRoutes,
+	Outlet,
+	Navigate,
 } from 'react-router-dom';
 
 function App() {
+	const routes = useRoutes([
+		{
+			path: '/',
+			element: <MainPage />,
+		},
+		{
+			path: 'users',
+			element: <UsersLotout />,
+			children: [
+				{ index: true, element: <UserListPage /> },
+				{
+					path: ':userId',
+					element: <Outlet />,
+					children: [
+						{ path: 'profile', element: <UserProfilePage /> },
+						{ path: 'edit', element: <EditUserPage /> },
+						{ index: true, element: <Navigate to="./profile" /> },
+						{ path: '*', element: <Navigate to="../profile" /> },
+					],
+				},
+			],
+		},
+		{ path: '*', element: <Navigate to="/" /> },
+	]);
 	return (
 		<div className="App">
-			<BrowserRouter>
-				<h1>App Layout</h1>
-				<NavLink to="/users">Users List Page</NavLink>
-				<Switch>
-					<Route path="/users" component={UsersLotout} />
-					<Route path="/" component={MainPage} />
-					<Redirect to="/" />
-				</Switch>
-			</BrowserRouter>
+			<h1>App Layout</h1>
+			<NavLink to="/users">Users List Page</NavLink>
+			{routes}
 		</div>
 	);
 }
@@ -29,33 +45,25 @@ function MainPage() {
 }
 
 function UsersLotout() {
-	const { path } = useRouteMatch();
+	//const { path } = useRouteMatch();
 	return (
 		<div>
 			<h1>Users Loyout</h1>
 			<NavLink to="/">Main Page</NavLink>
-			<Switch>
-				<Route
-					path={path + '/:userId/profile'}
-					component={UserProfilePage}
-				/>
-				<Route path={path + '/:userId/edit'} component={EditUserPage} />
-				<Route path={path} exact component={UserListPage} />
-				<Redirect from={path + '/:userId'} to={path + '/:userId/profile'} />
-			</Switch>
+			<Outlet />
 		</div>
 	);
 }
 
 function UserListPage() {
-	const { path } = useRouteMatch();
+	//const { path } = useRouteMatch();
 	return (
 		<div>
 			<h1>User List Page</h1>
 			<ul>
 				{new Array(5).fill('').map((_, index) => (
 					<li key={'user_list_component_' + index}>
-						<NavLink to={`${path}/${index}`}>User {index}</NavLink>
+						<NavLink to={index + '/profile'}>User {index}</NavLink>
 					</li>
 				))}
 			</ul>
